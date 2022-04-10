a89d2a7c3b6f3cb309ec65eef7395bc1dd9b671b
Rangel Reale <rangelspam@gmail.com>
2022-04-10 12:00:08 -0300

    error handling

--- src/Client.cpp
+++ src/Client.cpp
@@ -6,6 +6,7 @@
 
 #include <thread>
 #include <chrono>
+#include <sstream>
 
 namespace ecapplogcpp
 {
@@ -17,6 +18,9 @@ public:
 	Command getCommand() { return Command::Command_Quit; }
 };
 
+class ConnectionError : public std::runtime_error {
+	using std::runtime_error::runtime_error;
+};
 
 class Client::Impl
 {
@@ -42,7 +46,7 @@ public:
 		if (_isOpen)
 		{
 			_isRunning = false;
-			_queue.enqueue(std::make_shared<CmdQuit>());
+			_queue.reenqueue(std::make_shared<CmdQuit>()); // add at top
 			_thread.join();
 			_isOpen = false;
 		}
@@ -70,7 +74,7 @@ private:
 			}
 			catch (std::exception&)
 			{
-
+				// ignore
 			}
 			if (cont && _isRunning)
 				std::this_thread::sleep_for(std::chrono::seconds(5));
@@ -80,7 +84,13 @@ private:
 	bool handleConnectionLoop()
 	{
 		sockpp::tcp_connector conn({ _address, _port });
-		if (!conn) return true;
+		if (!conn.connect({ _address, _port })) {
+			std::stringstream err;
+			err << "Error connecting to server at "
+				<< sockpp::inet_address(_address, _port)
+				<< "\n\t" << conn.last_error_str();
+			throw ConnectionError(err.str());
+		}
 		conn.read_timeout(std::chrono::seconds(5));
 		handleBanner(conn);
 
@@ -89,11 +99,18 @@ private:
 			auto cmd = _queue.dequeue();
 			if (cmd)
 			{
-				auto quit = std::dynamic_pointer_cast<CmdQuit>(cmd);
-				if (quit) {
-					return false;
+				try {
+					auto quit = std::dynamic_pointer_cast<CmdQuit>(cmd);
+					if (quit) {
+						return false;
+					}
+					handleCmd(conn, cmd);
 				}
-				handleCmd(conn, cmd);
+				catch (ConnectionError &)
+				{
+					_queue.reenqueue(cmd);
+					throw;
+				}
 			}
 		}
 		return true;
@@ -116,14 +133,31 @@ private:
 	{
 		// write command
 		uint8_t ccmd = static_cast<uint8_t>(command);
-		conn.write(&ccmd, sizeof(command));
+		connWrite(conn, &ccmd, sizeof(command));
 
 		// write size
 		uint32_t size = htonl(static_cast<uint32_t>(data.size()));
-		conn.write(&size, sizeof(size));
+		connWrite(conn, &size, sizeof(size));
 
 		// write data
-		conn.write(data);
+		connWrite(conn, data.data(), data.size());
+	}
+
+	void connWrite(sockpp::tcp_connector& conn, const void* buf, size_t n)
+	{
+		if (conn.write(buf, n) != n)
+		{
+			if (conn.last_error() == EPIPE) {
+				throw ConnectionError("It appears that the socket was closed.");
+			}
+			else {
+				std::stringstream err;
+				err << "Error writing to the TCP stream ["
+					<< conn.last_error() << "]: "
+					<< conn.last_error_str();
+				throw ConnectionError(err.str());
+			}
+		}
 	}
 
 	std::string _appName, _address;
--- src/SafeQueue.h
+++ src/SafeQueue.h
@@ -25,14 +25,22 @@ public:
     void clear()
     {
         std::lock_guard<std::mutex> lock(m);
-        while (!q.empty()) q.pop();
+        while (!q.empty()) q.pop_front();
     }
 
     // Add an element to the queue.
     void enqueue(T t)
     {
         std::lock_guard<std::mutex> lock(m);
-        q.push(t);
+        q.push_back(t);
+        c.notify_one();
+    }
+
+    // Add an element to the queue at the top.
+    void reenqueue(T t)
+    {
+        std::lock_guard<std::mutex> lock(m);
+        q.push_front(t);
         c.notify_one();
     }
 
@@ -47,12 +55,12 @@ public:
             c.wait(lock);
         }
         T val = q.front();
-        q.pop();
+        q.pop_front();
         return val;
     }
 
 private:
-    std::queue<T> q;
+    std::deque<T> q;
     mutable std::mutex m;
     std::condition_variable c;
 };
